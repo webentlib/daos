0037bb97efc50396eab817409470695d82ed43fc
Maksim Koroteev <maksimkoroteev@ttk.ru>
2025-11-21 12:07:26 +0300

    YAC

--- forms.py
+++ forms.py
@@ -5,23 +5,48 @@ from apps.users.models import User
 
 
 class DaosAuthenticationForm(AuthenticationForm):
-    @staticmethod
-    def _check_failed_login_attempts(user):
-        if user and user.failed_login_attempts > 5:
-            raise ValidationError('Too many failed login attempts')
+    """
+    Каждый раз, когда происходит неудачного попытка входа в систему —
+    Django вызывает метод `get_invalid_login_error`.
+    Перехватываем этот метод, и записываем неудачные попытки входа соответствующему username.
+
+    Каждый раз, когда происходит удачная попытка входа в систему —
+    Django вызывает метод `confirm_login_allowed`, проверяющий на `is_active`.
+    Перехватываем этот метод, и проверяем также на `failed_login_attempts`.
+
+    ПОДКЛЮЧЕНИЕ
+
+    1. models.py:User
+
+    failed_login_attempts = models.IntegerField(default=0)
+    last_failed_login_attempt_at = models.DateTimeField(null=True, blank=True)
+
+    2. urls.py
+
+    from django.contrib.auth.views import LoginView
+    from apps.users.forms import DaosAuthenticationForm
+
+
+    urlpatterns = [
+        path('admin/login/', LoginView.as_view(authentication_form=DaosAuthenticationForm, template_name='admin/login.html'), name='login'),
+        ...
+    ]
+    """
 
     def get_invalid_login_error(self):
         username = self.cleaned_data.get('username')
         user = User.objects.filter(username=username).first()
-
         if user:
             user.failed_login_attempts += 1
             user.last_failed_login_attempt_at = timezone.now()
             user.save()
-
-        self._check_failed_login_attempts(user)
         return super().get_invalid_login_error()
 
     def confirm_login_allowed(self, user):
         self._check_failed_login_attempts(user)
         return super().confirm_login_allowed(user)
+
+    @staticmethod
+    def _check_failed_login_attempts(user):
+        if user and user.failed_login_attempts > 5:
+            raise ValidationError('Too many failed login attempts')
